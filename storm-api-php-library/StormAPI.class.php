4f8d59df792f11edb1a1bf9c664addcf45f6970f
mackdaddydiesel@gmail.com <mackdaddydiesel@gmail.com@d735aac6-0125-86d4-ea43-38411f5aa12f>
2013-01-08 16:36:04 +0000

    Added PHPDoc Documentation for the class
    git-svn-id: https://storm-api-php-library.googlecode.com/svn/trunk@4 d735aac6-0125-86d4-ea43-38411f5aa12f

--- storm-api-php-library/StormAPI.class.php
+++ storm-api-php-library/StormAPI.class.php
@@ -1,11 +1,23 @@
 <?php
-	/*
-	 * Author: Jason Gillman Jr.
-	 * Email: jgillman@liquidweb.com
-	 * Description: This is my attempt at writing a PHP wrapper that will ease Storm API calls with PHP
-	 * 				It will be designed to use the JSON format for talking with the API server.
-	 * 				$api_method is as described in docs (Case doesn't matter)
-	 * 				request() method returns an array generated from the API return
+	/**
+	 * This file contains the StormAPI Class for working with Liquid Web's Storm Platorm API
+	 * 
+	 * @package StormAPI
+	 * @license http://opensource.org/licenses/EPL-1.0 Eclipse Public License
+	 * @author Jason Gillman Jr <jgillman@liquidweb.com>
+	 * 
+	 */
+
+	/**
+	 * This class allows for making calls to the Storm Platform API.
+	 * 
+	 * The class will perform automatic encoding and decoding of JSON for passing and retrieving information from the Storm Platform API
+	 * The returned output is an array
+	 * View the Storm Platform API documentation for a listing of methods: http://www.liquidweb.com/StormServers/api/docs/v1/
+	 * 
+	 * @author Jason Gillman Jr <jgillman@liquidweb.com>
+	 * @package StormAPI
+	 *
 	 */
 	class StormAPI
 	{
@@ -13,6 +25,14 @@
 		private $api_user, $api_pass, $base_url, $api_format, $api_full_uri, $api_request;
 		private $api_request_body, $api_method, $api_params, $api_return, $api_version; 
 		
+		/**
+		 * 
+		 * @param string $api_user The Storm API User
+		 * @param string $api_pass The API User's Password
+		 * @param string $api_method The Storm API Method being called. Example: "server/list"
+		 * @param int $api_version The API version to use. Defaults to 1
+		 * 
+		 */
 		function __construct($api_user, $api_pass, $api_method, $api_version = "1")
 		{	
 			//$this->api_user = $api_user;
@@ -29,17 +49,36 @@
 			curl_setopt($this->api_request, CURLOPT_USERPWD, "$api_user:$api_pass"); // Pass the creds
 		}
 		
+		/**
+		 * 
+		 * @param string $parameter The parameter for the Storm API Method
+		 * @param string $value The value of the parameter
+		 * @return array Appends the parameter and value to the array being used for storing the API method's parameters
+		 * 
+		 */
 		function add_param($parameter, $value)
 		{
 			$this->api_request_body['params'][$parameter] = $value;
 		}
 		
+		/**
+		 * 
+		 * @return null Clears the array being used for storing the API method's parameters
+		 * 
+		 */
 		function clear_params()
 		{
 			unset($this->api_request_body);
 			curl_setopt($this->api_request, CURLOPT_HTTPGET, TRUE); //If the request was previously run with params, this cleans those out. Otherwise they go back with the request
 		}
 		
+		/**
+		 * 
+		 * @param string $api_method The new Storm API method you would like to use
+		 * @param boolean $clearparams Defaults TRUE - clears the array being used for storing the API method's parameters
+		 * @return null
+		 * 
+		 */
 		function new_method($api_method, $clearparams = TRUE) // Clears out parameters by default, since they may not apply now
 		{
 			if($clearparams == TRUE)
@@ -54,6 +93,13 @@
 			
 		}
 		
+		/**
+		 * 
+		 * This method makes the call to the Storm Platform API and retrieves the information
+		 * 
+		 * @return array Returns a JSON decoded array of returned information from the API call
+		 * 
+		 */
 		function request()
 		{
 			if(is_array($this->api_request_body)) // We have params
