c9e44e2401ba919cd2e5875c545324e8555eeae8
Jason Gillman Jr. <jgillman@liquidweb.com>
2013-08-20 05:24:21 -0400

    Removed the API_VERSION constant as it will toss warnings and potentially cause problems if you are trying to instantiate (or even unset and reinstantiate) a StormAPI object.
    Also updated the listMethods method to account for this (it just
    defaults to 'v1' since you can't pass a variable as a default value for
    the $docVersion parameter).

--- StormAPI.class.php
+++ StormAPI.class.php
@@ -42,7 +42,6 @@
 			$this->base_url = 'https://api.stormondemand.com/';
 			$this->api_format = 'json';
 			$this->api_port = 443;
-			define("API_VERSION", $api_version);
 			
 			$this->api_full_uri = $this->base_url . $api_version . "/" . $api_method . "." . $this->api_format;
 			$this->api_request = curl_init($this->api_full_uri); // Instantiate
@@ -136,10 +135,10 @@
 		 *
 		 * This method will return a list of available API methods
 		 *
-		 * @param string $docVersion The version of the API being used. Defaults to the version of the API specified upon construction
+		 * @param string $docVersion The version of the API being used. Defaults to 'v1'
 		 * @return array Returns an array of the available API methods based on the version supplied
 		 */
-		function listMethods($docVersion = API_VERSION)
+		function listMethods($docVersion = 'v1')
 		{
 			$this->api_docs = file_get_contents("http://www.liquidweb.com/StormServers/api/docs/" . $docVersion . "/docs.json");
 			$this->api_docs = json_decode($this->api_docs, TRUE);
