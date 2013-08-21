264e5730f49e10e672598d690034f37499011d62
Jason Gillman Jr. <jgillman@liquidweb.com>
2013-08-21 09:35:41 -0400

    Refactored variable and method names to use camelCaps

--- StormAPI.class.php
+++ StormAPI.class.php
@@ -22,33 +22,33 @@
 	class StormAPI
 	{
 		// Let's define attributes
-		private $api_user, $api_pass, $base_url, $api_format, $api_full_uri, $api_request;
-		private $api_request_body, $api_method, $api_params, $api_return, $api_version, $api_port; 
+		private $apiUser, $apiPass, $baseUrl, $apiFormat, $apiFullUri, $apiRequest;
+		private $apiRequestBody, $apiMethod, $apiParams, $api_return, $apiVersion, $apiPort; 
 		
 		/**
 		 * 
-		 * @param string $api_user The Storm API User
-		 * @param string $api_pass The API User's Password
-		 * @param string $api_method The Storm API Method being called. Example: "server/list"
-		 * @param int $api_version The API version to use. Defaults to v1
+		 * @param string $apiUser The Storm API User
+		 * @param string $apiPass The API User's Password
+		 * @param string $apiMethod The Storm API Method being called. Example: "server/list"
+		 * @param int $apiVersion The API version to use. Defaults to v1
 		 * 
 		 */
-		function __construct($api_user, $api_pass, $api_method, $api_version = "v1")
+		function __construct($apiUser, $apiPass, $apiMethod, $apiVersion = "v1")
 		{	
-			//$this->api_user = $api_user;
-			//$this->api_pass = $api_pass;
-			//$this->api_method = $api_method;
-			//$this->version = $api_version;
-			$this->base_url = 'https://api.stormondemand.com/';
-			$this->api_format = 'json';
-			$this->api_port = 443;
+			//$this->apiUser = $apiUser;
+			//$this->apiPass = $apiPass;
+			//$this->apiMethod = $apiMethod;
+			//$this->version = $apiVersion;
+			$this->baseUrl = 'https://api.stormondemand.com/';
+			$this->apiFormat = 'json';
+			$this->apiPort = 443;
 			
-			$this->api_full_uri = $this->base_url . $api_version . "/" . $api_method . "." . $this->api_format;
-			$this->api_request = curl_init($this->api_full_uri); // Instantiate
-			curl_setopt($this->api_request, CURLOPT_RETURNTRANSFER, TRUE); // Don't dump directly to output
-			curl_setopt($this->api_request, CURLOPT_PORT, $this->api_port); // The port to call to.
-			curl_setopt($this->api_request, CURLOPT_SSL_VERIFYPEER, TRUE); // It does look like verification works now.
-			curl_setopt($this->api_request, CURLOPT_USERPWD, "$api_user:$api_pass"); // Pass the creds
+			$this->apiFullUri = $this->baseUrl . $apiVersion . "/" . $apiMethod . "." . $this->apiFormat;
+			$this->apiRequest = curl_init($this->apiFullUri); // Instantiate
+			curl_setopt($this->apiRequest, CURLOPT_RETURNTRANSFER, TRUE); // Don't dump directly to output
+			curl_setopt($this->apiRequest, CURLOPT_PORT, $this->apiPort); // The port to call to.
+			curl_setopt($this->apiRequest, CURLOPT_SSL_VERIFYPEER, TRUE); // It does look like verification works now.
+			curl_setopt($this->apiRequest, CURLOPT_USERPWD, "$apiUser:$apiPass"); // Pass the creds
 		}
 		
 		/**
@@ -58,9 +58,9 @@
 		 * @return array Appends the parameter and value to the array being used for storing the API method's parameters
 		 * 
 		 */
-		function add_param($parameter, $value)
+		function addParam($parameter, $value)
 		{
-			$this->api_request_body['params'][$parameter] = $value;
+			$this->apiRequestBody['params'][$parameter] = $value;
 		}
 		
 		/**
@@ -68,30 +68,32 @@
 		 * @return null Clears the array being used for storing the API method's parameters
 		 * 
 		 */
-		function clear_params()
+		function clearParams()
 		{
-			unset($this->api_request_body);
-			curl_setopt($this->api_request, CURLOPT_HTTPGET, TRUE); //If the request was previously run with params, this cleans those out. Otherwise they go back with the request
+			unset($this->apiRequestBody);
+			$this->apiRequestBody['params'] = array(); // Initialize blank, so that a warning doesn't get thrown about the array being undefined
+			curl_setopt($this->apiRequest, CURLOPT_HTTPGET, TRUE); //If the request was previously run with params, this cleans those out. Otherwise they go back with the request
 		}
 		
 		/**
 		 * 
-		 * @param string $api_method The new Storm API method you would like to use
+		 * @param string $apiMethod The new Storm API method you would like to use
 		 * @param boolean $clearparams Defaults TRUE - clears the array being used for storing the API method's parameters
 		 * @return null
 		 * 
 		 */
-		function new_method($api_method, $clearparams = TRUE) // Clears out parameters by default, since they may not apply now
+		function newMethod($apiMethod, $clearparams = TRUE) // Clears out parameters by default, since they may not apply now
 		{
 			if($clearparams == TRUE)
 			{
-				unset($this->api_request_body);
-				curl_setopt($this->api_request, CURLOPT_HTTPGET, TRUE); //If the request was previously run with params, this cleans those out. Otherwise they go back with the request
+				unset($this->apiRequestBody);
+				$this->apiRequestBody['params'] = array(); // Initialize blank, so that a warning doesn't get thrown about the array being undefined
+				curl_setopt($this->apiRequest, CURLOPT_HTTPGET, TRUE); //If the request was previously run with params, this cleans those out. Otherwise they go back with the request
 			}
 			
-			$this->api_method = $api_method; // New method, coming right up!
-			$this->api_full_uri = $this->base_url . $this->api_method . "." .$this->api_format; // New URI since method change
-			curl_setopt($this->api_request, CURLOPT_URL, $this->api_full_uri);
+			$this->apiMethod = $apiMethod; // New method, coming right up!
+			$this->apiFullUri = $this->baseUrl . $this->apiMethod . "." .$this->apiFormat; // New URI since method change
+			curl_setopt($this->apiRequest, CURLOPT_URL, $this->apiFullUri);
 			
 		}
 		
@@ -101,34 +103,34 @@
 		 * 
 		 * @return string Returns a string containing the server, port, method, and parameters currently set
 		 */
-		function debug_info()
+		function debugInfo()
 		{
-			$this->debug_vars = "Full URI: " . $this->api_full_uri . "\n";
-			$this->debug_vars .= "Port: " . $this->api_port . "\n";
-			$this->debug_vars .= "Parameters as follows: \n";
+			$this->debugVars = "Full URI: " . $this->apiFullUri . "\n";
+			$this->debugVars .= "Port: " . $this->apiPort . "\n";
+			$this->debugVars .= "Parameters as follows: \n";
 			
-			if(isset($this->api_request_body))
+			if(isset($this->apiRequestBody))
 			{
-				foreach($this->api_request_body['params'] as $par_key => $par_value)
+				foreach($this->apiRequestBody['params'] as $par_key => $par_value)
 				{
 					if(is_array($par_value))
 					{
-						$this->debug_vars .= $par_key . "=>" . print_r($par_value, TRUE);
+						$this->debugVars .= $par_key . "=>" . print_r($par_value, TRUE);
 					}
 					else
 					{
-						$this->debug_vars .= $par_key . " => " . $par_value . "\n";
+						$this->debugVars .= $par_key . " => " . $par_value . "\n";
 					}
 				}
 			}
 			else
 			{
-				$this->debug_vars .= "No Parameters\n";
+				$this->debugVars .= "No Parameters\n";
 			}
 			
-			$this->debug_vars .= "=== End Params ===\n";
+			$this->debugVars .= "=== End Params ===\n";
 			
-			return $this->debug_vars;
+			return $this->debugVars;
 		}
 		
 		/**
@@ -140,10 +142,10 @@
 		 */
 		function listMethods($docVersion = 'v1')
 		{
-			$this->api_docs = file_get_contents("http://www.liquidweb.com/StormServers/api/docs/" . $docVersion . "/docs.json");
-			$this->api_docs = json_decode($this->api_docs, TRUE);
+			$this->apiDocs = file_get_contents("http://www.liquidweb.com/StormServers/api/docs/" . $docVersion . "/docs.json");
+			$this->apiDocs = json_decode($this->apiDocs, TRUE);
 		
-			foreach($this->api_docs as $groupName => $group)
+			foreach($this->apiDocs as $groupName => $group)
 			{
 				foreach($group['__methods'] as $methodName => $methodSpecs)
 				{
@@ -162,17 +164,17 @@
 		 */
 		function request()
 		{
-			if(is_array($this->api_request_body)) // We have params
+			if(is_array($this->apiRequestBody)) // We have params
 			{
-				curl_setopt($this->api_request, CURLOPT_POST, TRUE); //POST method since we'll be feeding params
-				curl_setopt($this->api_request, CURLOPT_HTTPHEADER, Array('Content-type: application/json')); // Since we'll be using JSON
-				curl_setopt($this->api_request, CURLOPT_POSTFIELDS, json_encode($this->api_request_body)); // Insert the parameters
+				curl_setopt($this->apiRequest, CURLOPT_POST, TRUE); //POST method since we'll be feeding params
+				curl_setopt($this->apiRequest, CURLOPT_HTTPHEADER, Array('Content-type: application/json')); // Since we'll be using JSON
+				curl_setopt($this->apiRequest, CURLOPT_POSTFIELDS, json_encode($this->apiRequestBody)); // Insert the parameters
 			}
 			
 			// Now send the request and get the return on investment
 			try
 			{
-				return json_decode(curl_exec($this->api_request), TRUE); // Pull the trigger and get nice pretty arrays of returned data
+				return json_decode(curl_exec($this->apiRequest), TRUE); // Pull the trigger and get nice pretty arrays of returned data
 			}
 			catch (Exception $e)
 			{
